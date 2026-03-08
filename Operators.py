02e4a89fdcb88d7e8b5e8673feccb2f2c81f365f
Unixxxxxx <sudhanshuroyss208@gmail.com>
2026-03-08 15:39:18 +0530

    Operators__^ ^ __-wq

---
+++ Operators.py
@@ -0,0 +1,57 @@
+a =11
+b = 7   
+print(a+b)
+print (a - b)
+print(a * b)
+print(a/b) 
+print(a // b)    #Remainder 
+print(a %b)
+print(a** b)
+
+
+print(f"-----------------------------------Assignment Operators_____^^__^^\n")
+x = 5
+y = 5
+A = 10
+B = 5
+c = 2
+D = 2
+E = 2
+A +2
+B *= 2
+c += 2
+D -= 2
+E /= 2
+print(f"__________\n--------------From Hear Operation are start to Perform to do task___--^^--^^")
+result = A+B
+sub = A-B
+mul = A*B
+div = A/B
+divs =A//B
+modulus =  A%B
+power = c**D
+print(f"______---------------------------------------------^^--^^--")
+print("\n")
+print(f"This is the value A=10 | +5 / ||", {A})
+print(f"The variable is B = 5 | B*  / || ", {B})
+print(f"The variable is c = 2 | c += /||",{c})
+print(f"The variable is D = 2 | D -= /||", {D} )
+print(f"The variable is E = 2 | E /= /||", {E})
+print(f"This is from result A+B | =  /||", {result})
+print(f"This is from sub  | A-B | =  /||",{sub})
+print(f"This is from mul  | A*B | =  /||", {mul})
+print(f"This is from / |divsion |and this will divide numbers and always return a float | A/B |  ||", {div})
+print(f"This is from // | division and thsi will returns this integer part only(No DECIMaL | A//B||", {divs})
+print(f"This is from %  | Return the remainder after division |  A%B                            ||", {modulus})
+print(f"This is from ** | Raises a number to a power          |   c**D                          ||", {power})
+print(f"------------------------------------------^^_ -^\n")
+
+print("\n")
+print(f"it will check if two value are equal                                    | v= x,y= 5  | == | ",(x == y))
+print(f"it will check if two value are not equal                                | v= x,y=5   | != | ",(x != y))
+print(f"it will check if the left value is greater than the righ value          | v= x,y=5   |  > | ",(x > y))
+print(f"it will check if the left value if smaller then the right value         | v= x,y=5   |  < | ",(x<y))
+print(f"it will check if the value is greater then OR EQual to another value    | v= x,y=5   | >= | ",(x>=y))
+print(f"it will check if the value is less then OR EQual to another value       | v= x,y=5   | <= | ",(x<=y))
+print(f"__________________________________________________________________________________________________-^__^__")
+print("\n")
